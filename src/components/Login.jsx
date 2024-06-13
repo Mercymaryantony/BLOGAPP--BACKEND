a0556b53458f34f1fd4303dcdceec87a2dd6e730
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-13 14:16:09 +0530

    changes made

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,21 @@
-import axios from 'axios'
 import React, { useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
-const Signup = () => {
+const Login = () => {
     const [data,setdata]=useState({
         "name":"",
-    "emailid":"",
-    "pass":""
+        "pass":""
     })
     const inputhandler = (event)=>{
         setdata({...data,[event.target.name]:event.target.value})
     }
     const readValue=()=>{
-        axios.post("http://localhost:8004/signup",data).then(
+        axios.post("http://localhost:8004/login",data).then(
             (response)=>{
-                if(response.data.status == "SIGNUP"){
-                    alert("ADDED")
+                if(response.data.status == "success"){
+                    alert("LOGGED IN")
                 }
                 else{
                     alert("FAILED")
@@ -28,6 +28,7 @@ const Signup = () => {
             }
         ).finally()
     }
+ 
   return (
     <div>
         <Navbar/>
@@ -36,19 +37,18 @@ const Signup = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"><b>NAME</b></label>
-                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputhandler}/>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"><b>EMAIL</b></label>
-                            <input type="text" className="form-control" name='emailid' value={data.emailid} onChange={inputhandler} />
+                            <label htmlFor="" className="form-label"><b>USERNAME</b></label>
+                            <input type="text" className="form-control"  name='name' value={data.name} onChange={inputhandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label"><b>PASSWORD</b></label>
-                            <input type="password" name="pass" value={data.pass} onChange={inputhandler} id="" className="form-control" />
+                            <input type="text" className="form-control" name='pass' value={data.pass} onChange={inputhandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-primary" onClick={readValue}>REGISTER</button>
+                            <button className="btn btn-success" onClick={readValue}>LOGIN</button>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                          <Link to='/signup'>  <button className="btn btn-primary">SIGNUP</button> </Link>
                         </div>
                     </div>
                 </div>
@@ -58,4 +58,4 @@ const Signup = () => {
   )
 }
 
-export default Signup
+export default Login
